4ccc89cee9dfd316882bed4938389ccf5b2e3b57
GamingRex4274 <grex4274@gmail.com>
2023-08-01 13:53:08 -0400

    Added bullets moving in the direction the ship faces.

---
+++ Asteroids/Bullet.cpp
@@ -0,0 +1,22 @@
+#include "Bullet.h"
+
+Bullet::Bullet(const Ship& ship)
+{
+	rect = sf::RectangleShape(sf::Vector2f(width, height));
+	const sf::Vector2f center = sf::Vector2f(width, height) / 2.0f;
+
+	rect.setOrigin(center);
+	rect.setPosition(ship.getPosition());
+	rect.setRotation(ship.getRotation());
+	direction = ship.getDirection();
+}
+
+void Bullet::update(float dt)
+{
+	rect.move(direction * speed * dt);
+}
+
+void Bullet::draw(sf::RenderWindow& rw)
+{
+	rw.draw(rect);
+}
---
+++ Asteroids/Bullet.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Ship.h"
+
+class Bullet
+{
+public:
+	Bullet(const Ship& ship);
+	void update(float dt);
+	void draw(sf::RenderWindow& rw);
+private:
+	static constexpr int width = 5;
+	static constexpr int height = 8;
+	static constexpr float speed = 500.0f;
+	sf::RectangleShape rect;
+	sf::Vector2f direction;
+};
